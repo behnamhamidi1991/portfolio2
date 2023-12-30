759cdadec7441e56a90da96478f4a6961ee40286
behnamhamidi1991 <eslp91@hotmail.com>
2023-12-30 13:29:00 +0330

    Homepage + responsive done

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,17 @@ export default function Home() {
   return (
     <main>
       <div className="top-container">
-        <span>Behnam Hamidi</span>
-        <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-cyan-500 font-black text-[50px]">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-cyan-300 text-xl tracking-widest font-semibold intro-text">
+          I&apos;m Behnam Hamidi and I work as a{" "}
+        </span>
+        <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-cyan-500 font-black text-[55px] my-3">
           Frontend Developer
         </h1>
-        <h2>What&apos;s up? Here you can discover my creative ideas ... </h2>
+        <h2>
+          {" "}
+          <span> What&apos;s up? </span> Here you can discover my creative ideas
+          ...{" "}
+        </h2>
       </div>
     </main>
   );
