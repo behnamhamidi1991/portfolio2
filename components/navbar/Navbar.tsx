2d55d4982263109d9d626e02d7fb8669601b46e3
behnamhamidi1991 <eslp91@hotmail.com>
2023-12-30 12:51:02 +0330

    Top part of the home page is in process

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./navbar.module.css";
+import Link from "next/link";
+import { BiSolidHomeAlt2 } from "react-icons/bi";
+import { BiSolidUser } from "react-icons/bi";
+import { BiLogoMicrosoft } from "react-icons/bi";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <Link href="/" className={styles.navItem}>
+        <BiSolidHomeAlt2 />
+      </Link>
+      <Link href="/" className={styles.navItem}>
+        <BiSolidUser />
+      </Link>
+      <Link href="/" className={styles.navItem}>
+        <BiLogoMicrosoft />
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
