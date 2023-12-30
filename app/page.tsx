5a22f5292b4722ce41be2325a0e6794eb547f987
behnamhamidi1991 <eslp91@hotmail.com>
2023-12-30 14:52:45 +0330

    Homepage is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <div className="top-container">
+      <motion.div
+        className="top-container"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-cyan-300 text-xl tracking-widest font-semibold intro-text">
           I&apos;m Behnam Hamidi and I work as a{" "}
         </span>
@@ -15,7 +23,24 @@ export default function Home() {
           <span> What&apos;s up? </span> Here you can discover my creative ideas
           ...{" "}
         </h2>
-      </div>
+      </motion.div>
+
+      <motion.div className="top-btnContainer">
+        <div className="btnContainer">
+          <Link
+            href="/about"
+            className="btn aboutBtn bg-gradient-to-r from-purple-700 to-cyan-600"
+          >
+            About Me
+          </Link>
+          <a href="/" className="btn cvBtn">
+            Download My CV
+          </a>
+          <Link href="/hireme" className="btn hireBtn">
+            Hire Me
+          </Link>
+        </div>
+      </motion.div>
     </main>
   );
 }
