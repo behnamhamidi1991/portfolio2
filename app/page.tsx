b569b2df23732aaa2faa043baf2868e9d6bca5e4
behnamhamidi1991 <eslp91@hotmail.com>
2023-12-30 16:08:19 +0330

    Remove Framer Motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
-"use client";
 import Image from "next/image";
-import { motion } from "framer-motion";
+
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <motion.div
-        className="top-container"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-      >
+      <div className="top-container">
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-cyan-300 text-xl tracking-widest font-semibold intro-text">
           I&apos;m Behnam Hamidi and I work as a{" "}
         </span>
@@ -23,9 +17,9 @@ export default function Home() {
           <span> What&apos;s up? </span> Here you can discover my creative ideas
           ...{" "}
         </h2>
-      </motion.div>
+      </div>
 
-      <motion.div className="top-btnContainer">
+      <div className="top-btnContainer">
         <div className="btnContainer">
           <Link
             href="/about"
@@ -40,7 +34,7 @@ export default function Home() {
             Hire Me
           </Link>
         </div>
-      </motion.div>
+      </div>
     </main>
   );
 }
