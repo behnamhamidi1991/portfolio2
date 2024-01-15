b1636de8655549d64916636e16ea5ae0e94a8657
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-15 04:03:44 +0330

    New Menu

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,18 +4,31 @@ import Link from "next/link";
 import { BiSolidHomeAlt2 } from "react-icons/bi";
 import { BiSolidUser } from "react-icons/bi";
 import { BiLogoMicrosoft } from "react-icons/bi";
+import { FaBlog } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <Link href="/" className={styles.navItem}>
-        <BiSolidHomeAlt2 />
+      <Link href="/about" className={styles.navItem}>
+        <BiSolidUser className={styles.icon} />
+        <span className={styles.navTitle}>About</span>
       </Link>
       <Link href="/about" className={styles.navItem}>
-        <BiSolidUser />
+        <FaBlog className={styles.icon} />
+        <span className={styles.navTitle}>Blog</span>
+      </Link>
+      <Link href="/" className={styles.navItem}>
+        <BiSolidHomeAlt2 className={styles.icon} />
+        <span className={styles.navTitle}>Home</span>
       </Link>
       <Link href="/projects" className={styles.navItem}>
-        <BiLogoMicrosoft />
+        <BiLogoMicrosoft className={styles.icon} />
+        <span className={styles.navTitle}>Projects</span>
+      </Link>
+      <Link href="/projects" className={styles.navItem}>
+        <IoLogoWhatsapp className={styles.icon} />
+        <span className={styles.navTitle}>Contact</span>
       </Link>
     </div>
   );
