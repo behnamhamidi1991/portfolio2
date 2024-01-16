3e7a80c1accdca9f537ca895f89de4de1e8f05b5
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-16 14:40:51 +0330

    Begin

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { projects } from "@/app/data";
+import styles from "./project.module.css";
 
 const Project = ({ params: { id } }: any) => {
   const project = projects.find((project) => project.id === id);
@@ -9,15 +10,29 @@ const Project = ({ params: { id } }: any) => {
   }
 
   return (
-    <div>
-      <h1>{project.title}</h1>
-      <Image
-        src={project.imgUrl}
-        width={400}
-        height={600}
-        alt={project.title}
-      />
-      <p>{project.description}</p>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <Image
+            src={project.imgUrl}
+            width={400}
+            height={600}
+            alt={project.title}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.btnContainer}>
+          <button className="bg-gradient-to-r from-purple-700 to-cyan-600">
+            Demo
+          </button>
+          <button className={styles.btnTwo}>Code</button>
+        </div>
+      </div>
+
+      <div className={styles.description}>
+        <h1>{project.title}</h1>
+        <p>{project.description}</p>
+      </div>
     </div>
   );
 };
